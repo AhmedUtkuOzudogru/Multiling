2ccfb15b1ddf81d3187d313ef4df749b281c349a
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-05-04 20:41:16 +0300

    Firebase Addition
    just sending data to firebase rest will come soon(Ahmed&Saljug)

--- app/src/main/java/com/example/multiling/MainActivity.java
+++ app/src/main/java/com/example/multiling/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.multiling;
 
-import android.os.Bundle;
 
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    private EditText input;
+    private Button btn;
+    private DatabaseReference rootDatabaseref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        input=findViewById(R.id.input);
+        btn=findViewById(R.id.btn);
+        rootDatabaseref= FirebaseDatabase.getInstance().getReference();
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String data=input.getText().toString();
+
+                rootDatabaseref.setValue(data);
+
+            }
         });
     }
 }
