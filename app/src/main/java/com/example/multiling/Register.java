aaabe066d28e0f28360b5418a900b37cdd4496d9
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-05-05 23:39:47 +0300

    Register
    Register is done with proper connections to
    Login

--- app/src/main/java/com/example/multiling/Register.java
+++ app/src/main/java/com/example/multiling/Register.java
@@ -1,24 +1,100 @@
 package com.example.multiling;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Register Class
+ * Register New Users with Firebase Authantication
+ * Code code by Ahmed and Saljug
+ * Xml done by Ibrahim
+ */
 public class Register extends AppCompatActivity {
 
+    TextInputEditText editTextEmail, editTextPassword, editTextPasswordAgain ;
+    Button registerButton , logInButton ;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        editTextEmail  = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextPasswordAgain = findViewById(R.id.password_Again);
+        registerButton = findViewById(R.id.register);
+        logInButton = findViewById(R.id.logInButton2);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, passwordAgain;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextEmail.getText());
+                passwordAgain = String.valueOf(editTextPasswordAgain.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this,"Enter email!", Toast.LENGTH_LONG).show();
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this,"Enter password!", Toast.LENGTH_LONG).show();
+                }
+                if(!passwordAgain.equals(password)){
+                    Toast.makeText(Register.this,"Passwords does not match!", Toast.LENGTH_LONG).show();
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(Register.this, "Authentication Created.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+            }
         });
     }
 }
