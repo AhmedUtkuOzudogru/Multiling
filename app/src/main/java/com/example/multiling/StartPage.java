abc12368cd9d8711c2086fc4e86b123285f402c3
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-05-09 01:28:13 +0300

    StartPage is Done With Some Errors

--- app/src/main/java/com/example/multiling/StartPage.java
+++ app/src/main/java/com/example/multiling/StartPage.java
@@ -1,15 +1,24 @@
 package com.example.multiling;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+/**
+ * StartPage Class
+ * Landing Ground For User
+ * Code dode by Ahmed  06/05/2024 00:22
+ * Xml done by Ibrahim
+ * TODO: Login Button is Not Centered
+ */
 public class StartPage extends AppCompatActivity {
-
+    Button loginButton, registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,22 @@ public class StartPage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        registerButton = findViewById(R.id.startPageButton3);
+        loginButton = findViewById(R.id.startPageButton1);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
         });
     }
 }
