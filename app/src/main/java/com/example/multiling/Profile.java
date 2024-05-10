e799a139935bc711f0bb47525f98df6d0d760092
Saljug Mammadli <saljug.mammadli@gmail.com>
2024-05-10 22:31:06 +0300

    Edit Profile and Firebase
    Created Edit Profile, Changed Some buttons, Worked on Firebase, Not completed yet

--- app/src/main/java/com/example/multiling/Profile.java
+++ app/src/main/java/com/example/multiling/Profile.java
@@ -1,6 +1,22 @@
 package com.example.multiling;
 
+import static android.content.Intent.ACTION_PICK;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+//instead of StartAcitivityForResult
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +24,92 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Profile extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> mGetContent;
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore firestore;
+    String userID;
+    Button profileButton;
+    ImageView profilePicture;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        /*
+        mGetContent = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+                            if (data != null && data.getData() != null) {
+                                profilePicture.setImageURI(data.getData());
+                            }
+                        }
+                    }
+                });
+*/
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        profilePicture = findViewById(R.id.profilePicture);
+        profileButton = findViewById(R.id.profileButton);
+
+
+
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EditProfile.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) { // Check if the returned result is from the gallery request
+            if (resultCode == Activity.RESULT_OK) {
+                Uri imageUri = data.getData();
+
+                profilePicture.setImageURI(imageUri);
+            }
+        }
+    }
+
+
+
+
 }
