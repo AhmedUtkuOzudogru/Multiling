6301173c07a041754f47cfb4d36672e5b67b0002
Saljug Mammadli <saljug.mammadli@gmail.com>
2024-05-10 10:30:02 +0300

    Error Fix

--- app/src/main/java/com/example/multiling/FlashCard.java
+++ app/src/main/java/com/example/multiling/FlashCard.java
@@ -8,44 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class Settings extends AppCompatActivity {
+public class FlashCard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_flashcard);
+        setContentView(R.layout.activity_flash_card);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setSelectedItemId(R.id.navigator_flashcard);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-
-            if (itemId == R.id.navigator_settings) {
-                // Handle settings navigation
-                return true;
-            } else if (itemId == R.id.navigator_profile) {
-                // Handle profile navigation
-                return true;
-            } else if (itemId == R.id.navigator_home) {
-                // Handle home navigation
-                return true;
-            } else if (itemId == R.id.navigator_flashcard) {
-                // Handle flashcard navigation
-                return true;
-            } else if (itemId == R.id.navigator_writingexercises) {
-                // Handle writing exercises navigation
-                return true;
-            }
-
-            return false;
         });
     }
 }
