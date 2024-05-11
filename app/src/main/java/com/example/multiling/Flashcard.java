2c24863ee90e2bf5f6f1c7326f2e503f5735f863
amirothm <amir.othman@ug.bilkent.tr>
2024-05-11 19:20:54 +0300

    The methods for generating the questions as well as the flashcard and the writing exercise constructors are done.

--- app/src/main/java/com/example/multiling/Flashcard.java
+++ app/src/main/java/com/example/multiling/Flashcard.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +21,48 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class FlashCard extends AppCompatActivity {
+    private String front;
+    private String[] questionComponents;
+    private String ID;
+    private String askedWord;
+    private String correctAnswer;
+    private String firstWrongAnswer;
+    private String secondWrongAnswer;
+
+    public FlashCard()
+    {
+        this.questionComponents = createFlashcard("easy_words.txt");//TODO:this should be changed to the var in the user class that will hold the file name
+        this.ID = questionComponents[0];
+        this.askedWord = questionComponents[1];
+        this.correctAnswer = questionComponents[2];
+        this.firstWrongAnswer = questionComponents[3];
+        this.secondWrongAnswer = questionComponents[4];
+    }
+
+    public String[] createFlashcard(String filename)//TODO: declare a filename var in the user class in accordance with the proficiency level
+    {
+        Random random = new Random();
+        String[] components = new String[5];
+        //this array will contain the id, question, correct answer, and 2 wrong answers respectively
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename)))
+        {
+            for(int i = 0; i < random.nextInt(100) - 1; ++i)
+            {
+                reader.readLine();
+            }
+            String line = reader.readLine();
+            components = line.trim().split(":");
+            for(String i : components)
+            {
+                i.trim();
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("No file with such name");
+        }
+        return components;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/multiling/WritingExercise.java
+++ app/src/main/java/com/example/multiling/WritingExercise.java
@@ -14,7 +14,55 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class WritingExercise extends AppCompatActivity {
+    private String[] questionComponents;
+    private String ID;
+    private String sentence;
+    private String noSuffixAnswer;
+    private String suffixAnswer;
+    private String firstWrongAnswer;
+    private String secondWrongAnswer;
+
+    public WritingExercise()
+    {
+        this.questionComponents = createWriting("easy_writing.txt");//TODO:this should be changed to the var in the user class that will hold the file name
+        this.ID = questionComponents[0];
+        this.sentence = questionComponents[1];
+        this.noSuffixAnswer = questionComponents[2];
+        this.suffixAnswer = questionComponents[3];
+        this.firstWrongAnswer = questionComponents[4];
+        this.secondWrongAnswer = questionComponents[5];
+    }
+
+    public String[] createWriting(String filename)
+    {
+        Random random = new Random();
+        String[] components = new String[6];
+        //this array will contain the id, sentence with a blank, correct answer without any suffixes, suffixed correct answer, and 2 wrong answers respectively
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename)))
+        {
+            for(int i = 0; i < random.nextInt(100) - 1; ++i)
+            {
+                reader.readLine();
+            }
+            String line = reader.readLine();
+            components = line.trim().split(":");
+            for(String i : components)
+            {
+                i.trim();
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("No file with such name");
+        }
+        return components;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
