5b8d3a1ada605d79984e96ccbc4de492e34c3449
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-05-12 00:37:49 +0300

    Minor Error fix

--- app/src/main/java/com/example/multiling/Profile.java
+++ app/src/main/java/com/example/multiling/Profile.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 //instead of StartAcitivityForResult
 
@@ -19,20 +21,28 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Profile extends AppCompatActivity {
 
-    private ActivityResultLauncher<Intent> mGetContent;
+    TextView userNameTextView, emailTextView, levelTextView;
 
-    FirebaseAuth mAuth;
-    FirebaseFirestore firestore;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
     String userID;
     Button profileButton;
     ImageView profilePicture;
@@ -44,21 +54,25 @@ public class Profile extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
 
-        /*
-        mGetContent = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == RESULT_OK) {
-                            Intent data = result.getData();
-                            if (data != null && data.getData() != null) {
-                                profilePicture.setImageURI(data.getData());
-                            }
-                        }
-                    }
-                });
-*/
+        firebaseAuth=FirebaseAuth.getInstance();
+        userID=firebaseAuth.getCurrentUser().getUid();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        profilePicture = findViewById(R.id.profilePicture);
+        profileButton = findViewById(R.id.profileButton);
+        userNameTextView = findViewById(R.id.userNameEdit);
+        levelTextView = findViewById(R.id.emailEdit);
+        emailTextView = findViewById(R.id.levelEdit);
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                userNameTextView.setText(value.getString("name") + " " + value.getString("surname"));
+                levelTextView.setText(value.getString("proficiencyLevel"));
+                emailTextView.setText(value.getString("email"));
+            }
+        });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -83,13 +97,49 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+        BottomNavigationView bottomNavigation = findViewById(R.id.profileNavigation);
+        bottomNavigation.setSelectedItemId(R.id.navigator_profile);
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navigator_home)
+                {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_profile)
+                {
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_settings)
+                {
+                    startActivity(new Intent(getApplicationContext(), Settings.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_flashcard)
+                {
+                    startActivity(new Intent(getApplicationContext(), FlashCard.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_writingexercises)
+                {
+                    startActivity(new Intent(getApplicationContext(), WritingExercise.class));
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) { // Check if the returned result is from the gallery request
+            if (resultCode == Activity.RESULT_OK) {
+                Uri imageUri = data.getData();
 
-
-
-
-
-
+                profilePicture.setImageURI(imageUri);
+            }
+        }
     }
 
 
@@ -97,4 +147,8 @@ public class Profile extends AppCompatActivity {
 
 
 
+
+
 }
+
+
