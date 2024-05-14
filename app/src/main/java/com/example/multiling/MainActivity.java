ad546317e169ad03d353c4b703ae45d5929ca2e8
Saljug Mammadli <saljug.mammadli@gmail.com>
2024-05-14 21:47:08 +0300

    Error Fix
    The app runs now

--- app/src/main/java/com/example/multiling/MainActivity.java
+++ app/src/main/java/com/example/multiling/MainActivity.java
@@ -6,15 +6,18 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,28 +31,27 @@ import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView userNameTextView, levelTextView, emailTextView;
+    TextView userNameTextView, levelTextView,emailTextView;
     FirebaseFirestore firebaseFirestore;
     FirebaseAuth firebaseAuth;
     String userID;
     StorageReference storageReference;
 
-    Button goToProfilePageButton, goToWritingExerciseButton, goToFlashcardButton,goToSettingsButton;
-    AppCompatImageButton goToNotificationsButton;
+    ImageView profilePicture;
+
+    Button goToProfilePageButton, goToWritingExerciseButton, goToFlashcardButton,goToSettingsButton, goToNotificationsButton;
 
 
 
 
 
-    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
-    @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
-        userID = firebaseAuth.getCurrentUser().getUid();
 
 
 
@@ -65,13 +67,14 @@ public class MainActivity extends AppCompatActivity {
         goToSettingsButton=findViewById(R.id.mainSettingsButton);
         goToProfilePageButton=findViewById(R.id.mainProfileButton);
         goToNotificationsButton=findViewById(R.id.mainNotificationsButton);
-        setContentView(R.layout.activity_main);
+        profilePicture=findViewById(R.id.mainProfileView);
+
 
         DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                userNameTextView.setText(value.getString("name") + " " + value.getString("surname"));
+                userNameTextView.setText(value.getString("name")+" "+value.getString("surname"));
                 levelTextView.setText(value.getString("proficiencyLevel"));
                 emailTextView.setText(value.getString("email"));
             }
@@ -83,6 +86,10 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(byte[] bytes) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
                 profilePicture.setImageBitmap(bitmap);
+
+            }
+        });
+
 
 
         goToProfilePageButton.setOnClickListener(new View.OnClickListener() {
@@ -96,11 +103,19 @@ public class MainActivity extends AppCompatActivity {
         goToNotificationsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NotificationPage.class);
+                Intent intent = new Intent(getApplicationContext(), NotificationPage.class);
                 startActivity(intent);
             }
         });
 
+        goToProfilePageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+
+            }
+        });
         goToWritingExerciseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,20 +124,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        goToFlashcardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Flashcard.class);
+                startActivity(intent);
 
-        goToWritingExerciseButton.setOnClickListener(v -> {
-            Intent intentWritingExercise = new Intent(MainActivity.this, WritingExercise.class);
-            startActivity(intentWritingExercise);
+            }
+        });
+        goToSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Settings.class);
+                startActivity(intent);
+
+            }
         });
 
-        goToFlashcardButton.setOnClickListener(v -> {
-            Intent intentFlashcard = new Intent(MainActivity.this, Flashcard.class);
-            startActivity(intentFlashcard);
-        });
-
-        goToSettingsButton.setOnClickListener(v -> {
-            Intent intentSettings = new Intent(MainActivity.this, Settings.class);
-            startActivity(intentSettings);
-        });
     }
 }
