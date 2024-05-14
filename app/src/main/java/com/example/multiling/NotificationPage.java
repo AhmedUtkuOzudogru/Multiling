321e2f4ed1b8a255cfa5e439b98e73c9a0e24be3
Can <cyildirim@ug.bilkent.edu.tr>
2024-05-14 10:46:14 +0300

    navigation bar added to notificationpage

--- app/src/main/java/com/example/multiling/NotificationPage.java
+++ app/src/main/java/com/example/multiling/NotificationPage.java
@@ -1,4 +1,5 @@
 package com.example.multiling;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -6,7 +7,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
-
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import androidx.annotation.NonNull;
 
 public class NotificationPage extends AppCompatActivity{
     private List<NotificationModel> notifications = new ArrayList<>();
@@ -14,6 +18,42 @@ public class NotificationPage extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.notificationNavigation);
+        bottomNavigation.setSelectedItemId(R.id.navigator_flashcard);
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                if (item.getItemId() == R.id.navigator_home)
+                {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_profile)
+                {
+                    startActivity(new Intent(getApplicationContext(), Profile.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_settings)
+                {
+                    startActivity(new Intent(getApplicationContext(), Settings.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_flashcard)
+                {
+                    startActivity(new Intent(getApplicationContext(), Flashcard.class));
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigator_writingexercises)
+                {
+                    startActivity(new Intent(getApplicationContext(), WritingExercise.class));
+                    return true;
+                }
+                return false;
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
